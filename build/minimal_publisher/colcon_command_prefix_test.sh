45f309ff6f27924eb84d212c7320058b238b9858
hesham1salem <h.salem@wakeb.tech>
2024-08-05 12:18:45 +0300

    rm build install

--- build/minimal_publisher/colcon_command_prefix_test.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/hesham/Pictures/newaction/install/minimal_publisher/share/minimal_publisher/package.sh"
--- build/minimal_publisher/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/foxy/src/gtest_vendor
-# Build directory: /home/hesham/Pictures/newaction/build/minimal_publisher/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
